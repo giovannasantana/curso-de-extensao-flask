0ab4069bedc416a21b4d508b8f5335d5ee00ca27
Giovanna <giovannamsantanaa@gmail.com>
2022-08-10 21:49:35 -0300

    Criando uma variavel e utilizando no alo.html

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app = Flask("projeto")
 
 @app.route("/")
 def ola_mundo():
-    return render_template("alo.html"), 200
+    # Criando variavel com meu nome
+    nome = 'Giovanna'
+
+    return render_template("alo.html", n=nome), 200
 
 
 app.run()
