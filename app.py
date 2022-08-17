2bce9a98d31207380e5316ed016c413f2ddac19a
Giovanna <giovannamsantanaa@gmail.com>
2022-08-17 19:46:22 -0300

    Criando uma nova rota teste

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 import os
 os.system('cls')
 
+# Instanciando um objeto
 app = Flask("projeto")
 
 
+# Criando uma rota raiz
 @app.route("/")
+# Declarando uma funcao
 def ola_mundo():
     # Criando variavel com meu nome
     nome = 'Giovanna'
@@ -16,6 +19,12 @@ def ola_mundo():
         {"nome": "Playstation", "preco": 1999.99}]
 
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
+
+
+# Criando uma nova rota teste
+@app.route("/teste")
+def funcao_teste():
+    return "Nova rota teste", 200  # Código http da requisicao
 
 
 app.run()
